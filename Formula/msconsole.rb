d6097b277814151d77841e50112271183120beca
Ben <benlaugherty@gmail.com>
2019-11-26 10:30:26 -0800

    [update] msconsole formula to support client package

--- Formula/msconsole.rb
+++ Formula/msconsole.rb
@@ -40,6 +40,16 @@ class Msconsole < Formula
     sha256 "b4607520a7c97be96be4ddc00f4b9dac65f47a45af4b4cd13ed5a8879641d646"
   end
 
+  resource "args" do
+    url "https://files.pythonhosted.org/packages/e5/1c/b701b3f4bd8d3667df8342f311b3efaeab86078a840fb826bd204118cc6b/args-0.1.0.tar.gz"
+    sha256 "a785b8d837625e9b61c39108532d95b85274acd679693b71ebb5156848fcf814"
+  end
+
+  resource "clint" do
+    url "https://files.pythonhosted.org/packages/3d/b4/41ecb1516f1ba728f39ee7062b9dac1352d39823f513bb6f9e8aeb86e26d/clint-0.5.1.tar.gz"
+    sha256 "05224c32b1075563d0b16d0015faaf9da43aa214e4a2140e51f08789e7a4c5aa"
+  end
+
   def install
     virtualenv_install_with_resources
   end
