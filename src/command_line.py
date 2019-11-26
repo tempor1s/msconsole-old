fdbb92f549b12127cac0acc5df8af26fc944beba
Ben <benlaugherty@gmail.com>
2019-11-25 20:14:51 -0800

    [version] Bro..

--- src/command_line.py
+++ src/command_line.py
@@ -10,7 +10,7 @@ def main():
     except IndexError:
         print('Please add an attendence token after `checkin`. Example: `checkin BRAVE`')
         exit()
-    usr = checkin.CheckIn()
+    usr = checkin.CheckIn(token)
     usr.run()
 
 if __name__ == '__main__':
