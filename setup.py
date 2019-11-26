db73e9b14b5d07f5791b9821ac5d54072f8cc915
Ben <benlaugherty@gmail.com>
2019-11-25 22:28:58 -0800

    [add] main.py and Formula folder for better organization [refactor] location of Formula script and msconsole. Also prep for version 2.0.0 and rename

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ description = """
 See `github repo <https://github.com/tempor1s/make-checkin>`_ for information.
 """
 
-VERSION = '1.2.9'
+VERSION = '2.0.0'
 
 setup(
     name='Make Checkin',
@@ -13,7 +13,7 @@ setup(
     author_email='benlaugherty@gmail.com',
     packages=find_packages(),
     entry_points={
-        'console_scripts': ['checkin=src.command_line:main']
+        'console_scripts': ['checkin=src.command_line:main', 'ms=src.msconsole:main']
     },
-    description='A CLI application that allows you to checkin to a MakeSchool class.'
+    description="Suite of tools that makes your MakeSchool life easier!"
 )
