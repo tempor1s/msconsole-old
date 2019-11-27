03496ab989a6f79ce92b1b8653da5551b681d0b2
imthaghost <imthaghost@protonmail.com>
2019-11-26 22:18:18 -0800

    [update] added extra arguments

--- src/modules/calendar.py
+++ src/modules/calendar.py
@@ -38,6 +38,7 @@ if __name__ == '__main__':
     arguments = docopt(__doc__, version=__version__)
     if arguments['<time>']:
         print(say_hello(arguments['<time>']))
+    elif arguments['<hello>']
     elif not len(sys.argv) > 1:
         print(__doc__)
     else:
