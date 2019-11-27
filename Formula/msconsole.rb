fd9125c3522874afeaf14a9cfc76ed87d2a0bea4
Ben <benlaugherty@gmail.com>
2019-11-26 20:09:18 -0800

    [add] ruby package for docopt

--- Formula/msconsole.rb
+++ Formula/msconsole.rb
@@ -50,6 +50,11 @@ class Msconsole < Formula
     sha256 "05224c32b1075563d0b16d0015faaf9da43aa214e4a2140e51f08789e7a4c5aa"
   end
 
+  resource "docopt" do
+    url "https://files.pythonhosted.org/packages/a2/55/8f8cab2afd404cf578136ef2cc5dfb50baa1761b68c9da1fb1e4eed343c9/docopt-0.6.2.tar.gz"
+    sha256 "49b3a825280bd66b3aa83585ef59c4a8c82f2c8a522dbe754a8bc8d08c85c491"
+  end
+
   def install
     virtualenv_install_with_resources
   end
