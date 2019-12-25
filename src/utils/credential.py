e76cdb1601659425def647b569eb14f8f6f1fe20
imthaghost <imthaghost@protonmail.com>
2019-12-25 00:12:44 -0800

    [update] comment _login in credential.py

--- src/utils/credential.py
+++ src/utils/credential.py
@@ -108,57 +108,57 @@ def _create_creds(key):
         print('\x1b[1;31m' + "Failed to store password." +  # red
                   '\x1b[0m')
                   
-def _login(s, key, email, password):
-     """Login to MakeSchool dashboard using email and password."""
-        # makeschool login url to post and get from
-        login_url = "https://www.makeschool.com/login"
-        # get the login HTML
-        dashboard = s.get(login_url)
+# def _login(s, key, email, password):
+#      """Login to MakeSchool dashboard using email and password."""
+#         # makeschool login url to post and get from
+#         login_url = "https://www.makeschool.com/login"
+#         # get the login HTML
+#         dashboard = s.get(login_url)
 
-        # check to see if the response is ok
-        if dashboard.status_code == 200:
-            # parse the HTML returing the dashboard document
-            dashboard_html = html.fromstring(dashboard.text)
-            # Grab hidden form fileds by looking through HTML XPath
-            hidden_inputs = dashboard_html.xpath(
-                r'//form//input[@type="hidden"]')
-            # inside the form look for name and value fields to get authenticity token
-            form = {x.attrib["name"]: x.attrib["value"] for x in hidden_inputs}
-            # set the form email value
-            form['user[email]'] = email
-            # set the form password value
-            form['user[password]'] = password
-            # setup post request to login url with new data inserted into form
-            response = s.post(login_url, data=form)
-        else:
-            # otherwise retry connection to server
-            print('\x1b[1;31m' + 'Retrying to connect to server')
-            try:
-                # HTTP retransmission to same urls
-                retransmission(session=s).get(login_url)
-            # if there is a connection error request the user to try a new url
-            # the url has changed in some way - we have to update
-            except ConnectionError:
-                print('Bad url request, try a different one by setting login url')
-            # catastrophic error, no idea what to do with this just bail
-            except requests.exceptions.RequestException as e:
-                # catastrophic error. bail.
-                print(e)
-                sys.exit(1)
+#         # check to see if the response is ok
+#         if dashboard.status_code == 200:
+#             # parse the HTML returing the dashboard document
+#             dashboard_html = html.fromstring(dashboard.text)
+#             # Grab hidden form fileds by looking through HTML XPath
+#             hidden_inputs = dashboard_html.xpath(
+#                 r'//form//input[@type="hidden"]')
+#             # inside the form look for name and value fields to get authenticity token
+#             form = {x.attrib["name"]: x.attrib["value"] for x in hidden_inputs}
+#             # set the form email value
+#             form['user[email]'] = email
+#             # set the form password value
+#             form['user[password]'] = password
+#             # setup post request to login url with new data inserted into form
+#             response = s.post(login_url, data=form)
+#         else:
+#             # otherwise retry connection to server
+#             print('\x1b[1;31m' + 'Retrying to connect to server')
+#             try:
+#                 # HTTP retransmission to same urls
+#                 retransmission(session=s).get(login_url)
+#             # if there is a connection error request the user to try a new url
+#             # the url has changed in some way - we have to update
+#             except ConnectionError:
+#                 print('Bad url request, try a different one by setting login url')
+#             # catastrophic error, no idea what to do with this just bail
+#             except requests.exceptions.RequestException as e:
+#                 # catastrophic error. bail.
+#                 print(e)
+#                 sys.exit(1)
 
-        # if the login was successful
-        if 'successfully' in response.text:
-            # Print that we signed in successfully.
-            print('\x1b[1;32m' + 'Signed in successfully.' + '\x1b[0m' + '\n')
-            # GraphQL query to get current users name and student email
-            query = "{ currentUser {name studentEmail} }"
-            # make request to makeschool and drill down to currentUser from data response
-            currentUser = graph_query(self.s, query)['data']['currentUser']
-            # print the users name and MS email so that they know they logged in successfully
-            print('Name: {}'.format(currentUser['name']))
-            print('MS Email: {}\n'.format(currentUser['studentEmail']))
-        else:
-            # the credentials are probably wrong
-            print('The credentials entered are incorrect.\n')
-            _create_creds()
-            _login()
+#         # if the login was successful
+#         if 'successfully' in response.text:
+#             # Print that we signed in successfully.
+#             print('\x1b[1;32m' + 'Signed in successfully.' + '\x1b[0m' + '\n')
+#             # GraphQL query to get current users name and student email
+#             query = "{ currentUser {name studentEmail} }"
+#             # make request to makeschool and drill down to currentUser from data response
+#             currentUser = graph_query(self.s, query)['data']['currentUser']
+#             # print the users name and MS email so that they know they logged in successfully
+#             print('Name: {}'.format(currentUser['name']))
+#             print('MS Email: {}\n'.format(currentUser['studentEmail']))
+#         else:
+#             # the credentials are probably wrong
+#             print('The credentials entered are incorrect.\n')
+#             _create_creds()
+#             _login()
